c2dd8fa4e3132b877f0d7b971638ffb968759341
Ify <Zasterify@gmail.com>
2020-07-11 22:20:39 -0400

    if bungalow

--- functions/house.py
+++ functions/house.py
@@ -2,7 +2,7 @@ def house_paid(bungalow, duplex, hut):
     '''
     To define the function called 'house_paid' with 3 arguments
     '''
-    
+    if bungalow and not duplex and not hut:  # if bungalow is true, duplex and hut are false
     
 
 
