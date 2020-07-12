edb17fc968a6650f74b1a8c55abdf25e7aa038ad
Ify <Zasterify@gmail.com>
2020-07-12 00:41:07 -0400

    values_max

--- functions/min & max.py
+++ functions/min & max.py
@@ -9,4 +9,6 @@ def maximum(lst):
     print ('The maximum is ',maximum)  # to print the answer
 
 minimum([0, 7, -1, 3])  # to print the values of list for minimum
+maximum([-13, 5, -10, 1])  # to print the values of list for maximum
+
 
