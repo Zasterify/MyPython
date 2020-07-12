bde924fa3adb9db01e26391d6e84f281a54732b4
Ify <Zasterify@gmail.com>
2020-07-12 03:21:19 -0400

    if bicycle

--- functions/vehicle.py
+++ functions/vehicle.py
@@ -4,4 +4,5 @@ if vehicle == 'cab':  # if you pick a cab for going to work
     print('It will take you to work for 15 minutes.')
 elif vehicle == 'bus':  # if you pick a bus for going to work
     print('It will take you there for 45 minutes.')
+elif vehicle == 'bicycle':  # if you pick a bicycle for going to work
 
