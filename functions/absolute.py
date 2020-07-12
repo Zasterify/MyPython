88391d8d4cb297ee5bb0109e8ff7a3e628e9f1a1
Ify <Zasterify@gmail.com>
2020-07-12 02:13:14 -0400

    print_three

--- functions/absolute.py
+++ functions/absolute.py
@@ -5,3 +5,4 @@ value2 = abs(-75.5)  # to convert float from -75.5 to 75.5
 print (value2)  # to print the value2
 
 value3 = abs(100)  # to convert integer from 100 to 100
+print (value3)  # to print the value3
