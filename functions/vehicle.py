6128b44135119fedf735876892ac0cf98d4aa896
Ify <Zasterify@gmail.com>
2020-07-12 03:19:32 -0400

    print_bus

--- functions/vehicle.py
+++ functions/vehicle.py
@@ -3,4 +3,5 @@ vehicle = input('Enter your transport: ')
 if vehicle == 'cab':  # if you pick a cab for going to work
     print('It will take you to work for 15 minutes.')
 elif vehicle == 'bus':  # if you pick a bus for going to work
+    print('It will take you there for 45 minutes.')
 
