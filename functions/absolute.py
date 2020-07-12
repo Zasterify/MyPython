dec94e4138fbe13eef1acc3d4c077ea7794bfbad
Ify <Zasterify@gmail.com>
2020-07-12 02:11:41 -0400

    print_two

--- functions/absolute.py
+++ functions/absolute.py
@@ -2,3 +2,4 @@ value1 = abs(-25)  # to convert integer from -25 to 25
 print (value1)  # to print the value1
 
 value2 = abs(-75.5)  # to convert float from -75.5 to 75.5
+print (value2)  # to print the value2
