af4f0358a8c1b6b2a458f6cb1fd028bddd09378e
Ify <Zasterify@gmail.com>
2020-07-12 03:17:30 -0400

    print_cab

--- functions/vehicle.py
+++ functions/vehicle.py
@@ -1,4 +1,5 @@
 vehicle = input('Enter your transport: ')  
 '''to discover transport as you like'''
 if vehicle == 'cab':  # if you pick a cab for going to work
+    print('It will take you to work for 15 minutes.')
 
