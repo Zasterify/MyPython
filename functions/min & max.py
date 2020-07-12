ac5f0d6d35ef6f987ae872fab58d84daa55a62f1
Ify <Zasterify@gmail.com>
2020-07-12 00:35:38 -0400

    print min_list

--- functions/min & max.py
+++ functions/min & max.py
@@ -1,4 +1,5 @@
 def minimum(lst):  
     '''to define the function called 'minimum' with 1 argument 'list' '''
     minimum = min(lst)  # to calculate the minimum to get answer
+    print ('The minimum is ',minimum)  # to print the answer
 
