ed2df1b82e7454f2d0312462206d2564821abe15
Ify <Zasterify@gmail.com>
2020-07-12 03:24:53 -0400

    nothing

--- functions/vehicle.py
+++ functions/vehicle.py
@@ -7,4 +7,5 @@ elif vehicle == 'bus':  # if you pick a bus for going to work
 elif vehicle == 'bicycle':  # if you pick a bicycle for going to work
     print('It will take you there for an hour.')
 else:  # if not, you have no options for going to work
+    print('Oops, you are late to work.')
 
