244cde71ae1955961ace569d7e85b9768f185762
Ify <Zasterify@gmail.com>
2020-07-12 12:44:29 -0400

    define_snack

--- functions/expense.py
+++ functions/expense.py
@@ -2,3 +2,6 @@ def apartment_cost(nights):
   '''to define the function called apartment'''
   return 25 * nights  # to know how much spent per nights
 
+def snack_price(snack):
+  ''' to define the funtion called snack_price'''
+
